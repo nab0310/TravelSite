efc130aaa320df9dc136df8733673313dbb0039f
Nick Behrens <behrensn@iastate.edu>
2016-10-31 21:09:00 -0500

    commit stuff

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name' => 'Example',
+        'address' => 'support@example.com',
+        'name' => 'Support',
     ],
 
     /*
