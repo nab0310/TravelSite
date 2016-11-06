992399cbb62a5a6639122326fc2a51557f9bb986
Nick Behrens <behrensn@iastate.edu>
2016-11-05 19:26:47 -0500

    I changed it to poopies

--- resources/views/restaurant.blade.php
+++ resources/views/restaurant.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.getNearby')
 
 @section('title')
-<div class="panel-heading">Restraunts Nearby!</div>
+<div class="panel-heading">Restraunts Poopies Nearby!</div>
 @endsection
 @section('script')
 <script>
